4c8aad28f8d4aa35b18e6387d24490c9d50da1a3
krylovgeorgii <krylov.georgii@gmail.com>
2017-06-13 05:15:47 +0300

    example using button

--- MyRIO/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "type.h"
-#include "pins.h"
-#include "time.h"
-#include "log.h"
-#include "robot.h"
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "MyRIO/ButtonIRQ.h"
+#include <thread>
+#include "MyRIO/button.h"
+#include "MyRIO/type.h"
+#include "MyRIO/pins.h"
+#include "MyRIO/time.h"
+#include "MyRIO/log.h"
+#include "MyRIO/robot.h"
+
+void constructor(int argc, char **argv);
+NiFpga_Status distructor();
+void buttonIrqThread();
+
+void buttonIrqThread() {
+	robot::button::waitButtonPush();
+	message("exit from button");
+	exit(distructor());
+}
+
+int main(int argc, char **argv){
+	constructor(argc, argv);
+
+	robot::button::waitButtonPush();
+	message("start doing");
+	wait(1);
+
+	std::thread buttonThread(buttonIrqThread);
+	buttonThread.detach();
+	wait(5);
+	message("END");
+
+
+	return distructor();
+}
+
+void constructor(int argc, char **argv){
+	robot::start(argc, argv);
+}
+
+NiFpga_Status distructor(){
+	return robot::finish();
+}
+
